c877e0e969a771bb15449a1831f16e1a6486ca64
Jakooob <sokoljakub14@gmail.com>
2023-02-23 12:35:37 +0100

    power and root

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 let answer;
+let oldAnswer;
+let result;
 
 $(".operations").children().click(function (){
     try{
-        if($(this).html() === "=") {
-            if (eval(answer) === Infinity){
-                answer += "=&#8734;";
-            } else{
-                answer += "=" + eval(answer);
+        switch ($(this).html()){
+            case "=":{
+                oldAnswer = answer;
+                if (eval(answer) === Infinity){
+                    answer += "=&#8734;";
+                    break;
+                }
+                // answer = answer.replaceAll('^', "**");
+                console.log("sdf")
+                console.log(answer.replaceAll(/(\d+)rt(\d+)/g, "Math.pow($1, 1/$2)"));;
+                result = eval(answer);
+                answer = oldAnswer + "=" + result;
+                break;
             }
-        }
-        else if ($(this).html() === "C") answer = '';
-        else{
-            if (answer === undefined || answer === '') answer = $(this).html();
-            else answer += $(this).html();
+            case "C":{
+                answer = '';
+                break;
+            }
+            case "⌫":{
+                answer = answer.slice(0, -1)
+                break;
+            }
+            default:{
+                if (answer === undefined || answer === '') answer = $(this).html();
+                else answer += $(this).html();
+                break;
+            }
         }
     } catch (error){
         answer = "Error";
