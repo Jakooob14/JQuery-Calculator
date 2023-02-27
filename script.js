5e74b227c89e3cd836a47baa369e3ec528b62615
Jakooob <sokoljakub14@gmail.com>
2023-02-27 22:59:59 +0100

    Fixed root function
    + Fixed root function
    + Comments
    + Made the result in a different element

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
-let answer;
+let calculation;
 let oldAnswer;
 let result;
 
 $(".operations").children().click(function (){
     try{
+        // What is clicked
         switch ($(this).html()){
             case "=":{
-                oldAnswer = answer;
-                if (eval(answer) === Infinity){
-                    answer += "=&#8734;";
-                    break;
+                // Save input for later use
+                oldAnswer = calculation;
+                // Replace all '^' with '**' so the eval function actually understands it
+                calculation = calculation.replaceAll('^', "**");
+                // Captures numbers between 'rt' so they can be rooted
+                calculation = calculation.replaceAll(/(\d+)rt(\d+)/g, "Math.pow($1, 1/$2)");
+                result = eval(calculation);
+                if (eval(calculation) === Infinity){
+                    result = "&#8734;";
                 }
-                // answer = answer.replaceAll('^', "**");
-                console.log("sdf")
-                console.log(answer.replaceAll(/(\d+)rt(\d+)/g, "Math.pow($1, 1/$2)"));;
-                result = eval(answer);
-                answer = oldAnswer + "=" + result;
+                // Replaces shown calculation with the before saved input so stuff like '^' doesn't appear as '**' to the user
+                calculation = oldAnswer;
                 break;
             }
             case "C":{
-                answer = '';
+                calculation = '';
+                result = '';
                 break;
             }
             case "⌫":{
-                answer = answer.slice(0, -1)
+                // Removes last character
+                calculation = calculation.slice(0, -1)
                 break;
             }
             default:{
-                if (answer === undefined || answer === '') answer = $(this).html();
-                else answer += $(this).html();
+                if (calculation === undefined || calculation === '') calculation = $(this).html();
+                else calculation += $(this).html();
                 break;
             }
         }
     } catch (error){
-        answer = "Error";
+        calculation = "Error";
     }
 
-    $("#answer").html(answer);
+    $("#calculation").html(calculation);
+    $("#answer").html(result);
 })
